457202d4b34f09292780a479b6f678bc048990bf
Sebastian Krauß <sebastian.krauss@open-xchange.com>
2025-04-23 15:48:41 +0200

    Removed anything related to OX Cloud, renamed as a consequence

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "oxcloud",
+    name: "appsuite",
     platforms: [
         .macOS(.v13)
     ],
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "oxcloud",
+            name: "appsuite",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]),
