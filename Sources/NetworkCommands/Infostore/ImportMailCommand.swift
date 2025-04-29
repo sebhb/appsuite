190578233d59b5ed5683aa5a014e2ec47865c9ce
Sebastian Krauß <sebastian.krauss@open-xchange.com>
2025-04-29 14:27:00 +0200

    Refactored multipart commands to same base command

--- Sources/NetworkCommands/Infostore/ImportMailCommand.swift
+++ Sources/NetworkCommands/Infostore/ImportMailCommand.swift
@@ -1,10 +1,9 @@
 import Foundation
 
-class ImportMailCommand: NetworkCommand<ImportMailResponse> {
+class ImportMailCommand: MultipartNetworkCommand<ImportMailResponse> {
 
     let session: RemoteSession
     let mailData: Data
-    let boundary: String = "----Boundary-\(UUID().uuidString)"
 
     init(session: RemoteSession, mailData: Data) {
         self.session = session
@@ -12,20 +11,8 @@ class ImportMailCommand: NetworkCommand<ImportMailResponse> {
         super.init(serverAddress: session.server)
     }
 
-    override func method() -> HTTPMethod {
-        .Post
-    }
-
-    override func postContentType() -> String {
-        return "multipart/form-data; boundary=" + boundary
-    }
-
     override func requestParameters() -> [String : String] {
         return ["action": "import", "folder": "default0/INBOX", "force": "true", "force_json_response": "true", "session": session.session]
-    }
-
-    override func usesRequestDictionary() -> Bool {
-        return false
     }
 
     override func requestData() -> Data {
