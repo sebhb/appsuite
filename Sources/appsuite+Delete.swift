3a1b560274f8a01944c7fe6f1fa4ac24445edd04
Sebastian Krauß <sebastian.krauss@open-xchange.com>
2025-05-07 14:47:31 +0200

    Number of years for which to delete appointments configurable

--- Sources/appsuite+Delete.swift
+++ Sources/appsuite+Delete.swift
@@ -11,11 +11,12 @@ extension Appsuite {
         static let configuration = CommandConfiguration(commandName: "appointments", abstract: "Delete all events in a user's calendar.", discussion: "Deletes all events in a user's calendar in the time range of one year in the past to one year in the future. This does NOT send mail notifications to participants.")
 
         @OptionGroup var userCredentialsOptions: UserCredentialsOptions
+        @OptionGroup var deleteOptions: DeleteAppointmentsOptions
 
         mutating func run() async throws {
             do {
                 let deleteEventsWorker = DeleteEventsWorker(userCredentialsOptions: userCredentialsOptions)
-                try await deleteEventsWorker.deleteEvents()
+                try await deleteEventsWorker.deleteEvents(years: deleteOptions.years)
             }
             catch {
                 print("An error occurred: \(error)")
